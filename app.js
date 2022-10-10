919e6fae4160f936baa9eae68d7593fd7ed4c314
Paul Thomas <paulthomas2107@gmail.com>
2022-10-10 20:53:42 +0100

    More...

--- app.js
+++ app.js
@@ -13,15 +13,37 @@ async function loadQuestions() {
 }
 
 function addCategory(category) {
-  const columm = document.createElement('div');
-  columm.classList.add('genre-column');
+  const column = document.createElement('div');
+  column.classList.add('genre-column');
 
+  // Category
   const genreTitle = document.createElement('div');
   genreTitle.classList.add('genre-title');
   genreTitle.innerText = category.category;
+  column.appendChild(genreTitle);
+  game.append(column);
 
-  columm.appendChild(genreTitle);
-  game.append(columm);
+  // Question
+
+  const card = document.createElement('div');
+  card.classList.add('card');
+  column.appendChild(card);
+  if (category.difficulty === 'easy') {
+    card.innerText = 100;
+  }
+  if (category.difficulty === 'medium') {
+    card.innerText = 200;
+  }
+  if (category.difficulty === 'hard') {
+    card.innerText = 300;
+  }
+
+  card.setAttribute('data-question', category.question);
+  card.setAttribute('data-answer-1', category.incorrectAnswers[0]);
+  card.setAttribute('data-answer-2', category.incorrectAnswers[1]);
+  card.setAttribute('data-answer-3', category.incorrectAnswers[2]);
+  card.setAttribute('data-correct', category.correctAnswer);
+  card.setAttribute('data-value', card.innerText);
 }
 
 // Main Loop start
